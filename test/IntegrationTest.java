c1c07e014f038d0e78ec76e3b3104c95dc8aad55
Anton Lunev <antonluneyv@gmail.com>
2016-10-27 22:45:47 +0300

    adding IT for auth

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,7 +1,11 @@
+import com.google.common.collect.Maps;
 import org.junit.*;
 
+import play.Application;
 import play.mvc.*;
 import play.test.*;
+
+import java.util.Map;
 
 import static play.test.Helpers.*;
 import static org.junit.Assert.*;
@@ -18,6 +22,23 @@ public class IntegrationTest {
     public void test() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
             browser.goTo("http://localhost:3333");
+            assertTrue(browser.pageSource().contains("Your new application is ready."));
+        });
+    }
+
+    @Test
+    @Ignore
+    public void testLoginWithGoogle() {
+        Map<String, String> config = Maps.newHashMap(inMemoryDatabase());
+        config.put("APP_SECRET", "dummy_secret");
+
+        System.setProperty("APP_SECRET", "dummy_secret");
+
+        Application app = fakeApplication(config);
+
+        running(testServer(3333, app), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:3333/authenticate/google");
+
             assertTrue(browser.pageSource().contains("Your new application is ready."));
         });
     }
